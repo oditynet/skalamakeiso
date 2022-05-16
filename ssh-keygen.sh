22b5f3094501a0ba03957d8f550f42260a8be427
odity <oditynet@gmail.com>
2022-05-16 12:42:19 +0300

    add ssh-keygen.sh

---
+++ ssh-keygen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+count=5
+name="node0"
+ssh-keygen -b 2048 -t rsa -f /root/.ssh/migrate -N ""
+
+for (( i=1; i<= $count; i++ )); do
+ echo $name$i
+ key=`cat /root/.ssh/migrate.pub|awk -F ' ' '{print $2}'`
+ echo "ssh-rsa " $key" root@"$name$i   >>/root/.ssh/authorized_keys
+done
+for (( i=1; i<= $count; i++ )); do
+ echo $name$i
+ `scp /root/.ssh/migrat* /root/.ssh/authorized_keys root@$name$i:/root `
+done
